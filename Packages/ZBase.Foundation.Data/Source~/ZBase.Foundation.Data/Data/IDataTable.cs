c6471cdfd3fb3e4a5141f3f47b045304fddd6b98
Grashaar <grashaar@outlook.com>
2023-05-14 21:45:49 +0700

    Add constraint on Id to IDataTable interface

--- Packages/ZBase.Foundation.Data/Source~/ZBase.Foundation.Data/Data/IDataTable.cs
+++ Packages/ZBase.Foundation.Data/Source~/ZBase.Foundation.Data/Data/IDataTable.cs
@@ -1,6 +1,20 @@
 ﻿namespace ZBase.Foundation.Data
 {
-    public interface IDataTable<T> where T : IData
+    public interface IDataTable<T> where T : IData { }
+
+    public interface IDataTable<TId, TData>
+        where TData : IData
+    { }
+
+    public interface IGetData<TId, TData> : IDataTable<TId, TData>
+        where TData : IData
     {
+        TData GetData(TId id);
+    }
+
+    public interface ITryGetData<TId, TData> : IDataTable<TId, TData>
+        where TData : IData
+    {
+        bool TryGetData(TId id, out TData data);
     }
 }
